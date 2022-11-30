3f9a930b4513505678a71b3b5108db5fc2370668
Mohammad Danish <mohddanish8299@gmail.com>
2022-11-30 18:44:01 +0530

    show current time

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-// fetch unsplash API
+const currentTimeEl = document.getElementById('currentTimeEl')
+
 fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
     .then(res => res.json())
     .then(data => {
-        // get image
         let backgroundImage = data.urls.regular
         document.body.style.backgroundImage = `url(${backgroundImage})`
-        // get authors name 
         const imageAuthor = `By: ${data.user.name}`
     })
     .catch(err => {
         document.body.style.backgroundImage =
             'url(http://placekitten.com/1000/1000)'
     })
+
+setInterval(() => {
+    const date = new Date()
+    const currentTime = date.toLocaleTimeString('en-US', {
+        timeStyle: 'short',
+    })
+    currentTimeEl.querySelector('span').textContent = currentTime
+        .replace("AM", "").replace("PM", "")
+}, 1000)
