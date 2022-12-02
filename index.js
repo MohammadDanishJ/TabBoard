cce916f18e01d3f6d882be76f14741012b32a1b5
Mohammad Danish <mohddanish8299@gmail.com>
2022-12-02 19:50:16 +0530

    show date and day

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage =
             'url(http://placekitten.com/1000/1000)'
     })
-
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 setInterval(() => {
     const date = new Date()
     const currentTime = date.toLocaleTimeString('en-US', {
         timeStyle: 'short',
     })
-    currentTimeEl.querySelector('span').textContent = currentTime
-        .replace("AM", "").replace("PM", "")
+    currentTimeEl.querySelector('span.time').textContent = currentTime.replace("AM", "").replace("PM", "")
+    currentTimeEl.querySelector('span.date').textContent = `${weekday[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
 }, 1000)
